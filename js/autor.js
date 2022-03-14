ca76b73e62cd02d1a36dc36532f8fe3596aafb5f
Facundo Cordoba <fscordoba@gylgroup.com>
2022-03-14 17:02:56 -0300

    se sube los cambios  Crud autor

--- js/autor.js
+++ js/autor.js
@@ -1,4 +1,4 @@
-import {options, urlAutor } from "./constantes.js";
+import {options, urlAutor ,activar ,desactivar,footerModal,footerModalFormulario} from "./constantes.js";
 import {obtenerJson } from "./asincronico.js";
 
 
@@ -19,15 +19,35 @@ function obtenerAutores(url){
 obtenerJson(url).then(autores => {
   autores.forEach(autor => {
     
-    $template.querySelector(".name").textContent = autor.nombre;
-    $template.querySelector(".status").textContent = autor.alta;
-    $template.querySelector(".ver").dataset.nombre = autor.nombre;
-    $template.querySelector(".estado").dataset.estado = autor.alta;
+    $template.querySelector(".nombre").textContent = autor.nombre;
+    $template.querySelector(".nombre").id = `nombre_${autor.id}`;
 
-    /*$template.querySelector(".edit").dataset.id = el.id;
-    $template.querySelector(".edit").dataset.name = el.nombre;
-    $template.querySelector(".edit").dataset.status = el.constelacion;*/
+    $template.querySelector(".estado").textContent = autor.alta;
+    $template.querySelector(".estado").id = `estado_${autor.id}`;
+
+    $template.querySelector(".ver").dataset.nombre = autor.nombre;
+
+
+    $template.querySelector(".botonEstado").id = `botonEstado_${autor.id}`;
+
+    $template.querySelector(".botonEstado").classList.remove('btn-success');
+    $template.querySelector(".botonEstado").classList.remove('btn-danger');
+
+    $template.querySelector(".botonEstado").dataset.id= autor.id;
+    $template.querySelector(".botonEstado").dataset.estado = autor.alta;
+
+
+
+    if(autor.alta){
+      $template.querySelector(".botonEstado").classList.add('btn-danger');
+      $template.querySelector(".botonEstado").textContent="Desactivar";
+    }else{
+      $template.querySelector(".botonEstado").classList.add('btn-success');
+      $template.querySelector(".botonEstado").textContent="Activar";
+    }
+
     let $clone = d.importNode($template, true);
+     
     $fragment.appendChild($clone);
   });
   $table.querySelector("tbody").appendChild($fragment);
@@ -37,15 +57,28 @@ obtenerJson(url).then(autores => {
 
 function activarAutor(url,index){
    obtenerJson(url+index).then(response => {
-    { console.log("aqui se aplica la logica");
-   console.table(response);
+    { 
+      d.querySelector(`#estado_${index}`).textContent = true;
+      d.querySelector(`#botonEstado_${index}`).dataset.estado = true;
+      d.querySelector(`#botonEstado_${index}`).classList.toggle("btn-success");
+      d.querySelector(`#botonEstado_${index}`).classList.toggle("btn-danger");
+      d.querySelector(`#botonEstado_${index}`).textContent="Desactivar";
+      d.querySelector(".modal-body").innerHTML= `${response.message}`;
+      $myModal.show();
+    
  }});
 }
 
  function desactivarAutor(url, index){
   obtenerJson(url+index).then(response => {
-    { console.log("aqui se aplica la logica");
-   console.table(response);
+    { 
+     d.querySelector(`#estado_${index}`).textContent = false;
+     d.querySelector(`#botonEstado_${index}`).dataset.estado = false;
+     d.querySelector(`#botonEstado_${index}`).classList.toggle("btn-success");
+     d.querySelector(`#botonEstado_${index}`).classList.toggle("btn-danger");
+     d.querySelector(`#botonEstado_${index}`).textContent="Activar";
+     d.querySelector(".modal-body").innerHTML= `${response.message}`;
+     $myModal.show();
  }});
 }
 
@@ -83,36 +116,35 @@ form.addEventListener("submit", function(e){
   d.addEventListener("click", async e => {
     if (e.target.matches(".ver")) { 
       d.querySelector(".modal-body").innerHTML= `Autor: ${e.target.dataset.nombre}`;
+      d.querySelector(".modal-footer").innerHTML= footerModal;
       $myModal.show();
     
     }
     
-
     if (e.target.matches(".editar")) { 
-      d.querySelector(".modal-body").innerHTML= `Autor: ${e.target.dataset.nombre}`;
+      d.querySelector(".modal-body").innerHTML =`<form>
+                 <div class="mb-3">
+                 <label for="recipient-name" class="col-form-label">Recipient:</label>
+                <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="mb-3">
+              <label for="message-text" class="col-form-label">Message:</label>
+              <textarea class="form-control" id="message-text"></textarea>
+              </div>
+            </form>`;
+
+      d.querySelector(".modal-footer").innerHTML= footerModalFormulario;
       $myModal.show();
-    
     }
-
-    if (e.target.matches(".estado")) { 
-
-      if(e.target.dataset.estado){
-        // desactivar
-         //desactivarAutor();
-        alert(e.target.dataset.estado);
-
+    if (e.target.matches(".botonEstado")) { 
+      if(e.target.dataset.estado === "true"){
+       desactivarAutor(urlAutor+desactivar,e.target.dataset.id);
       }else{
-        //activar
-        //activarAutor();
-        alert(e.target.dataset.estado);
-
-      }
-          
-
-
-      d.querySelector(".modal-body").innerHTML= `aqui agregro la logica para el cambio de estado`;
+        activarAutor(urlAutor+activar,e.target.dataset.id);
+      }  
+      
+      d.querySelector(".modal-footer").innerHTML= footerModal;
       $myModal.show();
-    
     }
 
   /*
