ae562c0cc29e96bd30e61a54cf4b9ef419534152
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-03-18 09:36:39 -0300

    Se adiciona funcion de busqueda

--- js/editorial.js
+++ js/editorial.js
@@ -341,3 +341,21 @@ function modificarEditorial(id, options) {
   }).catch(error => console.error(error));
 }
 //Fin EDITAR
+
+let searchInput = document.getElementById('buscar');
+let table = document.getElementById("tabla").tBodies[0];
+let texto
+
+searchInput.addEventListener('keyup', buscaTabla);
+
+function buscaTabla() {
+  texto = searchInput.value.toLowerCase();
+  var r = 0;
+  let row
+  while (row = table.rows[r++]) {
+    if (row.children[0].innerText.toLowerCase().indexOf(texto) !== -1) {
+      row.style.display = null;
+    } else
+      row.style.display = 'none';
+  }
+}
